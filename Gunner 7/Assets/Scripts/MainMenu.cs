1b5adcacfee14e4c56eaeb306b53be670d32293d
odieman32 <dettermanbrodie@gmail.com>
2024-12-01 20:35:27 -0500

    Added map, main menu

---
+++ Gunner 7/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Start Game!");
+        //Loads the sceane corresponding by number
+        SceneManager.LoadSceneAsync(1);
+    }
+}
+
+//by Brodie Detterman
